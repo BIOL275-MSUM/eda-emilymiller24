59d20f29005c0f21ba4cdb0a20f70b7ef5f9a313
Emily Miller <pp3352cw@go.minnstate.edu>
2021-04-13 17:31:14 -0500

    Update 4

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,15 @@
 
+# Loading Packages --------------------------------------------------------
+
+
 library(tidyverse)
 library(readxl)
+library(sf)
+library(maps)
+
+
+# Loading Data Sets -------------------------------------------------------
+
 
 Poverty <- read_excel("Data/Entire Population Living in Poverty .xlsx") %>% 
   filter(LocationType == "County") %>% 
@@ -8,6 +17,10 @@ Poverty <- read_excel("Data/Entire Population Living in Poverty .xlsx") %>%
 
 Unemprate1 <- read_excel("Data/Ann_UnempRate_NSA (1).xlsx") # first 50 counties
 Unemprate2 <- read_excel("Data/Ann_UnempRate_NSA (2).xlsx") # second batch
+
+
+# Combining Data Sets -----------------------------------------------------
+
 
 unemployment <-
   Unemprate1 %>% 
@@ -19,7 +32,6 @@ unemployment <-
   print()
 
 
-
 Poverty<- select(Poverty, TimeFrame, Location, Data) %>%   #1. Get rid of LocationType and Percent
   rename(year=TimeFrame, county=Location, poverty=Data) #2. Rename other columns to year, county and poverty
 
@@ -29,14 +41,15 @@ unemployment <- unemployment %>%
 
 
 unemployment <- unemployment %>% 
-  mutate(county=str_replace_all(county, "Saint Louis", "St. Louis"))
+  mutate(county=str_replace_all(county, "Saint Louis", "St Louis"))
+
+
+Poverty <- mutate(Poverty, county=str_replace_all(county, "St. Louis", "St Louis"))
 
 
 poverty <- Poverty %>% 
   mutate(year=as.integer(year),
          poverty=as.numeric(poverty)) #4. Convert year and data to numbers as.numeric() for data, integer for year
-
-
 
 
 pe <- poverty %>% 
@@ -45,5 +58,84 @@ pe <- poverty %>%
                                       #6. by = c("year", "county")
 
 
+# Making Maps -------------------------------------------------------------
 
 
+map1 <- map_data("county") %>% 
+  as_tibble()
+
+
+map2 <-filter(map1, region=="minnesota")
+
+
+ggplot()+
+  geom_polygon(data=map2, aes(x=long, y=lat, group=group),
+               color="black", fill="lightblue")
+
+
+merge1 <- rename(pe, subregion=county)
+
+
+merge2 <- mutate(merge1, subregion=str_to_lower(subregion))
+
+
+mergedcounties <- inner_join(map2, merge2, by= "subregion")
+
+
+mergedcounties %>% 
+  filter(year == 2000) %>% 
+  ggplot() + 
+  geom_polygon(
+    mapping = aes(x=long, y=lat, group=group, fill = poverty), 
+    color = "white", size = 0.2
+  ) +
+  coord_map("albers", lat0 = 43.5, lat1 = 49) +
+  scale_fill_viridis_c(
+    limits = c(0.03,0.3),
+    direction = -1
+  ) +
+  theme(
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.background = element_blank()
+  )
+
+
+mergedcounties %>% 
+  filter(year == 2012) %>% 
+  ggplot() + 
+  geom_polygon(
+    mapping = aes(x=long, y=lat, group=group, fill = unemployment), 
+    color = "white", size = 0.2
+  ) +
+  coord_map("albers", lat0 = 43.5, lat1 = 49) +
+  scale_fill_viridis_c(
+    limits = c(2,15),
+    direction = -1
+  ) +
+  theme(
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.background = element_blank()
+  )
+
+
+
+# Correlation -------------------------------------------------------------
+
+cor(
+  mergedcounties$poverty,
+  mergedcounties$unemployment
+)
+
+ggplot(mergedcounties,
+       aes(x = poverty, y = unemployment, color = year)) +
+  geom_point() +
+  scale_fill_viridis_c(
+    limits = c(0.03,0.3),
+    direction = -1
+  )
+
+mergedcounties
